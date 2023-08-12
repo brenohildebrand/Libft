a75d60ace2b30fe8c22db201cce92a583aecf67e
Breno Hildebrand <hello@brenohildebrand.com>
2023-08-12 15:26:29 -0300

    improve memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 16:33:35 by bhildebr          #+#    #+#             */
-/*   Updated: 2023/08/11 16:47:27 by bhildebr         ###   ########.fr       */
+/*   Updated: 2023/08/12 15:25:37 by bhildebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memmove(void *dest, const void *src, unsigned long n)
 	if (dest == ((void *)0) && src == ((void *)0))
 		return ((void *)0);
 	index = 0;
-	if ((char *)dest < (char *)src)
+	if (dest < src)
 	{
 		while (index < n)
 		{
